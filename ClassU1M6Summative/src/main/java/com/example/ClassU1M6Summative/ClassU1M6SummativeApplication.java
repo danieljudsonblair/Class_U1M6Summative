4259a28574178f1714fb77ddd3ec2f756986ce30
s1191 <danieljudsonblair@gmail.com>
2019-07-12 10:44:33 -0400

    initial project commit

---
+++ ClassU1M6Summative/src/main/java/com/example/ClassU1M6Summative/ClassU1M6SummativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClassU1M6Summative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassU1M6SummativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassU1M6SummativeApplication.class, args);
+	}
+
+}
