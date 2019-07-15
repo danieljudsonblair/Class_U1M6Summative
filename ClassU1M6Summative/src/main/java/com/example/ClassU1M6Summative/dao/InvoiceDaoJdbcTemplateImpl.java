26756f2535da138e1871fc1ee2f466e77b32617d
Sara <smoujah1@uncc.edu>
2019-07-15 13:50:10 -0400

    fix invoice

--- ClassU1M6Summative/src/main/java/com/example/ClassU1M6Summative/dao/InvoiceDaoJdbcTemplateImpl.java
+++ ClassU1M6Summative/src/main/java/com/example/ClassU1M6Summative/dao/InvoiceDaoJdbcTemplateImpl.java
@@ -17,7 +17,7 @@ public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao{
     private JdbcTemplate jdbcTemplate;
 
     public static final String INSERT_INVOICE_SQL =
-            "insert into invoice(invoice_id, customer_id, order_date, pickup_date, return_date, late_fee) values (?,?,?,?,?,?)";
+            "insert into invoice(customer_id, order_date, pickup_date, return_date, late_fee) values (?,?,?,?,?)";
 
     public static final String SELECT_INVOICE_SQL =
             "select * from invoice where invoice_id = ?";
@@ -26,10 +26,10 @@ public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao{
             "select * from invoice";
 
     public static final String UPDATE_INVOICE_SQL =
-            "update customer set customer_id = ?, order_date = ?, pickup_date = ?, return_date = ?, late_fee = ? where invoice_id = ?";
+            "update invoice set invoice_id = ?, order_date = ?, pickup_date = ?, return_date = ?, late_fee = ? where invoice_id = ?";
 
     public static final String DELETE_INVOICE_SQL =
-            "delete from customer where customer_id = ?";
+            "delete from invoice where invoice_id = ?";
 
     @Autowired
     public InvoiceDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
@@ -43,7 +43,7 @@ public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao{
 
         int id = jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);
 
-        invoice.setCustomerID(id);
+        invoice.setInvoiceID(id);
         return invoice;
     }
 
