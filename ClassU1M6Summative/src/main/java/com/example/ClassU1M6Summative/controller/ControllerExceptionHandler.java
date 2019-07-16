e66a95d9dc5696bb93dd1a26e6f45875ad106ee8
mjvanvelsor <mjvanvelsor@gmail.com>
2019-07-16 15:21:28 -0400

    exception handler added

---
+++ ClassU1M6Summative/src/main/java/com/example/ClassU1M6Summative/controller/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.ClassU1M6Summative.controller;
+
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+@RequestMapping(produces = "application/vnd.error+json")
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<VndErrors> outOfRangeException(IllegalArgumentException e, WebRequest request){
+        VndErrors error = new VndErrors(request.toString(), e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+}
